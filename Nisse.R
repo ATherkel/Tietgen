b3efbddc7056a2bdd11098a700e0f1d120fa2b84
ATherkel <at.bmxrider@gmail.com>
2015-11-09 15:29:28 +0100

    Update Nisse.R

--- Nisse.R
+++ Nisse.R
@@ -4,13 +4,15 @@ nisse <- function(name){
              "William","Josephine","Anders","Emilie",
              "Morten Skjalm","Kristina")
     
-    source("http://dl.dropbox.com/s/ar8wjgt0mdhl9cu/Pincodes.R",
+    source("https://raw.githubusercontent.com/ATherkel/Tietgen/master/Pincodes.R",
            local = TRUE)
     
+    # Set seed for replication
     set.seed(100)
+    # How to handle if/else commands
+    msrpw <- "numeric"
     
-    masterpassword <- "Forsikring"
-    
+    # Vector with element entry numbers
     all.seq <- seq_along(all)
     
     ## Pair the elements from the 'all' list to another element
@@ -25,10 +27,10 @@ nisse <- function(name){
     cat("Type your 4 digit password to verify yourself")
     line <- readline()
     
-    if(line == masterpassword | 
+    if(line == msrpwd | 
            suppressWarnings(as.integer(line) == pincode[which(name == all)])){
         
-        if(line == masterpassword) name <- all 
+        if(line == msrpwd) name <- all 
 
         print(paste(name, ", you are the elf of ",
                     all[paired.with][all == name],"!",sep = ""))
