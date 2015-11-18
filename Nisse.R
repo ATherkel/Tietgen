b959659a5ae7ccd56125c9272bb8ee9a827adc29
ATherkel <at.bmxrider@gmail.com>
2015-11-18 20:35:45 +0100

    Update Nisse.R

--- Nisse.R
+++ Nisse.R
@@ -4,10 +4,13 @@ nisse <- function(name,mypin){
              "William","Josephine","Anders","Jasmin","Emilie",
              "Morten Gade","Morten Skjalm","Kristina")
     ## Capitalize first letter and make all other letters lower case
-    name.out <- paste(toupper(substring(name,1,1)),
-                      tolower(substring(name,2)),sep = "")
+    name.out <- {
+        s <- strsplit(name, " ")[[1]]
+        paste(toupper(substring(s, 1,1)), substring(s, 2),
+              sep="", collapse=" ")
+    }
     
-    if(!any(toupper(name) == toupper(unlist(strsplit(all," "))))){
+    if(!any(grepl(toupper(name), toupper(all)))){
         cat(paste("Sorry, ", name.out,
                   ", you are not on the Secret Santa list. :(",
                   sep = ""))
@@ -18,12 +21,15 @@ nisse <- function(name,mypin){
     #          ssl.verifypeer=0L, followlocation=1L)
     #eval(parse(text=url))
     
-    set.seed(1)
+    set.seed(1042)
     pincode <- floor(runif(length(all),1e3,1e4-1))
-    
+    ## Make sure pincodes are not duplicated
+    while(any(duplicated(pincode))){
+        pincode <- floor(runif(length(all),1e3,1e4-1))
+    }
     
     # Set seed for replication
-    set.seed(100)
+    set.seed(18)
     # How to handle if/else commands
     msrpw <- "numeric"
     
@@ -40,8 +46,7 @@ nisse <- function(name,mypin){
     # print(all[paired.with][all == name])
     
     #cat("Type your 4 digit password to verify yourself")
-    #line <- readline()
-    line <- mypin
+    
     
     # If we have not yet started, tell the user!
     if(Sys.Date() < "2015-11-18"){
@@ -56,16 +61,21 @@ nisse <- function(name,mypin){
             toupper(all.listed[[x]][y])
         })
     })
-
-    if(line == msrpw) {
+    
+    if(mypin == msrpw) {
         cat(paste(all," was the Secret Santa of ", all[paired.with],
                   "!\n",sep = ""))
-    } else if(suppressWarnings(as.integer(line) == 
-                                   pincode[grep(toupper(name),
-                                                all.listed.cap)])){
-        cat(paste(all[grep(toupper(name),all.listed.cap)],
+    } else if(any(suppressWarnings(as.integer(mypin) == 
+                                       pincode[grep(toupper(name),
+                                                    all.listed.cap)]))){
+        ## Take only the name that matches with the entered pincode
+        paired.with.pin <- which(
+            grep(toupper(name), all.listed.cap) == match(TRUE,pincode == mypin))
+        
+        cat(paste(all[grep(toupper(name),all.listed.cap)][paired.with.pin],
                   ", you are the Secret Santa of ",
-                  all[paired.with][grep(toupper(name),all.listed.cap)],
+                  all[paired.with][
+                      grep(toupper(name),all.listed.cap)][paired.with.pin],
                   "!",sep = ""))
     } else {
         cat("Pincode wrong.")
